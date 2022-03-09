090a44ffe2278f7f81d6f9b66c8b879a60c99cdf
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-03-09 12:14:08 +0100

    Integracion con la base de datos Mongo

--- Servidor/ProductManager/src/server.js
+++ Servidor/ProductManager/src/server.js
@@ -1,18 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(YOUR_MONGODB_URL,
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Error...", err);
+    process.exit();
+  });
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/status', (req, res) => {
-    res.json({"message": "El servidor se está ejecutando correctamente"});
+app.get("/status", (req, res) => {
+  res.json({ message: "200 OK", status: "El servidor se esta ejecutando correctamente"} );
 });
 
-let PORT = 3001
+let PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+  console.log(`Server is listening on port ${PORT}`);
