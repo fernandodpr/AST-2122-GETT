1bb97a56576ea83900ad69bf2d969dc018a9da2e
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-03-09 11:32:12 +0100

    Server.js

---
+++ Servidor/ProductManager/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+const monk = require('monk');
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
