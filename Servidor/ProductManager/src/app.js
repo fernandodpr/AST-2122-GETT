f7a977ad65cc6687861002070c14949ee785b3c2
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-03-20 13:48:41 -0700

    Cors allow all request

--- Servidor/ProductManager/src/app.js
+++ Servidor/ProductManager/src/app.js
@@ -1,12 +1,13 @@
 ///////////////////////////////////////////////////////////
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app =express();
 //////////////////////////////////////////////////////////////
 //Enviroment variables
-app.set('port', process.env.PMBACKPORT || 4000);
+app.set('port', process.env.PMBACKPORT || 3001);
 
-
+app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.json())
