2b1877c925a500bd8c338ba742ac7d88df341597
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-03-20 07:46:48 -0700

    API REST V.1 lista

--- Servidor/ProductManager/src/database.js
+++ Servidor/ProductManager/src/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/ProductManager',{
     useUnifiedTopology : true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useFindAndModify: false
 })
     .then((db) => console.log("Base de Datos Conectada"))
     .catch((error)=> console.error(error));
