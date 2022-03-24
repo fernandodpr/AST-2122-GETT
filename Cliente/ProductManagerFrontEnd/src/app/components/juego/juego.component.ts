ddbcdf5e14b02a69711cdad1ba0312b516176262
acallvazq <korreoalba@gmail.com>
2022-03-24 21:49:33 +0100

    Implementado funciones crud, edit y delete. Cambios en interfaz grafica

--- Cliente/ProductManagerFrontEnd/src/app/components/juego/juego.component.ts
+++ Cliente/ProductManagerFrontEnd/src/app/components/juego/juego.component.ts
@@ -1,15 +1,18 @@
+/*
+  CONTIENE LOS METODOS QUE VAN A SER LLAMADOS DESDE LA PAGINA HTML
+*/
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { NgForm } from "@angular/forms";
+import {Game} from 'src/app/models/game';
 
 @Component({
   selector: 'app-juego',
   templateUrl: './juego.component.html',
   styleUrls: ['./juego.component.css']
 })
-export class JuegoComponent implements OnInit {
 
-  
+export class JuegoComponent implements OnInit {
 
   constructor(public ProductService: ProductService) { }
 
@@ -20,14 +23,51 @@ export class JuegoComponent implements OnInit {
   getProducts(){
     this.ProductService.getProducts().subscribe(
       res => {
+        console.log(res);
         this.ProductService.juegos = res;
       },
-      err => console.log(err) 
-    ) 
+      err => console.log(err)
+    )
   }
 
+  resetForm(form: NgForm){
+    if(!form.value._id) form.reset();
+    else alert("Modo edicion. Limpiar el formulario está deshabilitado");
+  }
+
+  //agrega un juego desde el formulario
   addGame(form: NgForm){
-    console.log("A");
-    console.log(form.value);
+    if(form.value._id){
+      this.ProductService.updateProduct(form.value).subscribe(
+        (res) => console.log(res),
+        (err) => console.error(err)
+      );
+    }else {
+      console.log(form.value);
+      this.ProductService.createProduct(form.value).subscribe(
+        (res: any) => {
+          this.getProducts();
+          form.reset();
+        },
+        (err: any) => console.log(err)
+      );
+    }
+  }
+
+  //Elimina juego dado la id
+  deleteGame(id: string){
+    if(confirm('Este producto se va eliminar. ¿Confirma la operacion?')){
+      this.ProductService.deleteProduct(id).subscribe(
+        (res) => {
+          this.getProducts();
+         },
+         (err) => console.error(err)
+      );
+    }
+  }
+
+  //Editar los datos del juego desde el formulario
+  editGame(game: Game){
+    this.ProductService.selectedGame = game;
   }
 }
--- Cliente/ProductManagerFrontEnd/src/app/services/product.service.ts
+++ Cliente/ProductManagerFrontEnd/src/app/services/product.service.ts
@@ -1,22 +1,46 @@
+/*
+  METODOS USADOS DE FORMA INDIRECTA POR LA PAGINA HTML EN JUEGOCOMPONENT
+*/
 import {Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Game} from '../models/game';
 
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
   constructor(private http: HttpClient){}
-  URL_API = 'http://localhost:3001/api/product'
-  
-//  selectedGame: Game;
-  juegos: Game[] = [];
+  URL_API = 'http://localhost:3001/api/product';
 
+  selectedGame: Game = {
+    nombre: "",
+    stock: 0,
+    categoria: "",
+    precio: 0,
+    estudio: "",
+    lanzamiento: 0,
+    pegi: 0,
+    plataforma: "",
+    img: "",
+    _id: ""
+  };
+
+  juegos: Game[] = [];
 
   getProducts(){
     return this.http.get<Game[]>(this.URL_API);
   }
 
+  createProduct(game: Game){
+    return this.http.post(this.URL_API, game);
+  }
 
+  updateProduct(game: Game){
+    return this.http.post(`${this.URL_API}/${game._id}`, game);
+  }
+
+  deleteProduct(_id: string){
+    return this.http.delete(`${this.URL_API}/${_id}`);
+  }
 }
