78201d1682e48aa84794789c5b16011ab52776ac
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-04-01 01:16:45 -0700

    Cambio de Array de categoría a String Simple
    Para esta entrega por facilidad hemos cambiado de array de categorías a String

--- Servidor/ProductManager/src/models/product.model.js
+++ Servidor/ProductManager/src/models/product.model.js
@@ -3,12 +3,12 @@ const {Schema, model} = require('mongoose');
 const productSchema = new Schema({
     nombre: {type: String, required: true},
     stock: {type: Number, required: true},
-    categoria: [{type: String}],
+    categoria: {type: String, required: true},
     precio: {type: Number, required: true},
     estudio: {type: String, required: true},
     lanzamiento: {type: Date, required: true},
     pegi: {type: Number, required: true},
-    plataforma: [{type: String, required: true}],
+    plataforma: {type: String, required: true},
     img: {type:String, required: true}
     
 },{
