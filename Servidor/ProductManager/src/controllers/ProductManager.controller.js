de422e3c8dc57dee04ea46616b4accb1a9c357a5
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-04-03 05:09:34 -0700

    Gestión de errores en el servidor
    Congelado proyecto entrega 1

--- Servidor/ProductManager/src/controllers/ProductManager.controller.js
+++ Servidor/ProductManager/src/controllers/ProductManager.controller.js
@@ -6,38 +6,67 @@ ProductManagerCtrl.status = (req, res) => res.send('OK');
 
 //Todo por ID
 ProductManagerCtrl.getProduct = async (req, res) => {
-    const producto = await Product.findOne({_id: req.params.id})
-    res.send(producto);
+    try{
+        const producto = await Product.findOne({_id: req.params.id})
+        res.send(producto);
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 
 }
 ProductManagerCtrl.deleteProduct = async (req, res) => {
-    await Product.findOneAndDelete({_id: req.params.id})
-    res.send({message: '200 - OK'})
+    try{
+
+        await Product.findOneAndDelete({_id: req.params.id});
+        res.send({message: '200 - OK'})
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 }
 ProductManagerCtrl.editProduct = async (req, res) => {
-    await Product.findOneAndUpdate({_id: req.params.id},req.body)
-    res.send({message: '200 - OK'})
+    try{
+        await Product.findOneAndUpdate({_id: req.params.id},req.body)
+        res.send({message: '200 - OK'})
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 
 }
 //Genericos
 ProductManagerCtrl.createProduct = async (req, res) => {
-    const newProduct = new Product(req.body)
-    await newProduct.save()
-    res.send({message: '200 - OK'})
+    try{
+        const newProduct = new Product(req.body)
+        await newProduct.save()
+        res.send({message: '200 - OK'})
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 
 }
 ProductManagerCtrl.getProducts = async (req, res) => {
-   const products = await Product.find()
-   res.json(products)
+    try{
+        const products = await Product.find()
+        res.json(products)
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 }
 ProductManagerCtrl.deleteProducts = async (req, res) => {
-    await Product.deleteMany();
-    res.send('OK');
+    try{
+        await Product.deleteMany();
+        res.send('OK');
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 }
 
 ProductManagerCtrl.getCategory = async (req,res) =>{
-    const producto = await Product.find({categoria: req.params.name})
-    res.send(producto);
+    try{
+        const producto = await Product.find({categoria: req.params.name})
+        res.send(producto);
+    }catch(error){
+        res.send({message: 'Server error'})
+    }
 }
 
 
