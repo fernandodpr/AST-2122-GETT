2787778df9892ccb996551d1cd312715ac8eec77
fernandodpr <44924063+fernandodpr@users.noreply.github.com>
2022-04-29 16:28:55 +0200

    Username para que no sea tan cutre

--- Servidor/AuthManager/src/models/user.model.js
+++ Servidor/AuthManager/src/models/user.model.js
@@ -1,7 +1,8 @@
 const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
-    rol: {type: String, required: true}
+    rol: {type: String, required: true},
+    username: {type: String, required: false}
 },{
     timestamps: true,
     versionKey: false,
